570e8254d20b7682afe39c8fe4945509f64cfe68
Clowo <34722832+Clowo@users.noreply.github.com>
2019-07-02 22:05:45 +0300

    Add files via upload

---
+++ ssid_finder.py
@@ -0,0 +1,11 @@
+import os
+
+interface = input("interface: ")
+result = os.popen("sudo iw {} scan ".format(interface)).read()
+resultList = result.split("\n")
+SSID = list()
+for i in resultList:
+    if "SSID" in i:
+        SSID.append(i[6:])
+        
+print(SSID)
